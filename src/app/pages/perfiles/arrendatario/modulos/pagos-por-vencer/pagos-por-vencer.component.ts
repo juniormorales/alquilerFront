ac65af940cdde189f5b95edbcb14f0b43a09593e
= <jose.condori5@unmsm.edu.pe>
2020-07-08 18:16:04 -0500

    Se realizó el modal para subida de boleta de pago

--- src/app/pages/perfiles/arrendatario/modulos/pagos-por-vencer/pagos-por-vencer.component.ts
+++ src/app/pages/perfiles/arrendatario/modulos/pagos-por-vencer/pagos-por-vencer.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-pagos-por-vencer',
   templateUrl: './pagos-por-vencer.component.html',
-  styles: [
-  ]
+  styleUrls: ['./pagos-por-vencer.component.scss']
 })
 export class PagosPorVencerComponent implements OnInit {
 
-  constructor() { }
+  modalRef: BsModalRef;
+
+  constructor(private modalService: BsModalService) { }
 
   ngOnInit(): void {
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
 
 }
