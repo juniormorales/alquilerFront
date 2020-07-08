96d6be88c246c0d8a52fdeb4c57c8e0a4b8b4810
katriel <katriel181920@gmail.com>
2020-07-08 16:18:08 -0500

    pagos-por-aceptar-v02

--- src/app/pages/perfiles/arrendero/modulos/pago-por-aceptar/visualizar-recibo/visualizar-recibo.component.ts
+++ src/app/pages/perfiles/arrendero/modulos/pago-por-aceptar/visualizar-recibo/visualizar-recibo.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ISolicitudPropiedad } from 'src/models/ISolicitudPropiedad';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+
+//katriel
+import { IPago } from '../../../../../../../models/IPago';
 @Component({
   selector: 'app-visualizar-recibo',
   templateUrl: './visualizar-recibo.component.html',
@@ -10,6 +13,11 @@ export class VisualizarReciboComponent implements OnInit {
 
   
   input_sol_prop: ISolicitudPropiedad;
+
+  //katriel
+
+  input_pago:IPago;
+
   constructor( private modalService : BsModalService,
     private bsModalRef : BsModalRef) { }
 
--- src/app/services/common/modal-visualizar-recibo.service.ts
+++ src/app/services/common/modal-visualizar-recibo.service.ts
@@ -1,14 +1,11 @@
 import { Injectable } from '@angular/core';
-
 import { Observable } from 'rxjs';
 import { ModalOptions, BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
-import { ISolicitudPropiedad } from 'src/models/ISolicitudPropiedad';
-import { VerDetalleSolicitudComponent } from 'src/app/pages/perfiles/arrendero/modulos/solicitudes/ver-detalle-solicitud/ver-detalle-solicitud.component';
-import { EditarSolicitudComponent } from 'src/app/pages/perfiles/arrendatario/modulos/sol-hechas/editar-solicitud/editar-solicitud.component';
-import { RechazarSolicitudComponent } from 'src/app/pages/perfiles/arrendero/modulos/solicitudes/rechazar-solicitud/rechazar-solicitud.component';
 
+//katriel
 import { VisualizarReciboComponent } from 'src/app/pages/perfiles/arrendero/modulos/pago-por-aceptar/visualizar-recibo/visualizar-recibo.component';
-
+import { IPago } from '../../../models/IPago';
+//
 
 @Injectable({
   providedIn: 'root'
@@ -20,40 +17,12 @@ export class ModalVisualizarReciboService {
   constructor(
     private bsModalService: BsModalService
   ) { }
-/*
-  modalVerDetalleSolicitante(obj: ISolicitudPropiedad): Observable<any> {
-    const config: ModalOptions = {
-      initialState:  {
-        input_sol_prop: obj
-      },
-      animated: true,
-      ignoreBackdropClick: true,
-      backdrop: "static",
-      keyboard: false
-    }
-    this.modalRef = this.bsModalService.show(VerDetalleSolicitudComponent, config);
-    return new Observable<any>(observer => {
-      const subscripcion = this.bsModalService.onHidden.subscribe((reason: any) => {
-        if(reason === "CERRAR"){
-          observer.error();
-        }else{
-          observer.complete();
-        }
-      });
 
-      return {
-        unsubscribe() {
-          subscripcion.unsubscribe();
-        }
-      }
-    });
-  }
-  */
-
- modalVerDetalleSolicitante(obj: ISolicitudPropiedad): Observable<any> {
+ //katriel
+  modalVerRecibo(obj: IPago): Observable<any> {
   const config: ModalOptions = {
     initialState:  {
-      input_sol_prop: obj
+      input_pago: obj
     },
     animated: true,
     ignoreBackdropClick: true,
@@ -77,62 +46,6 @@ export class ModalVisualizarReciboService {
     }
   });
 }
-
-/*
-  modalEditarSolicitud(obj: ISolicitudPropiedad): Observable<any> {
-    const config: ModalOptions = {
-      initialState:  {
-        input_sol_prop: obj
-      },
-      animated: true,
-      ignoreBackdropClick: true,
-      backdrop: "static",
-      keyboard: false
-    }
-    this.modalRef = this.bsModalService.show(EditarSolicitudComponent, config);
-    return new Observable<any>(observer => {
-      const subscripcion = this.bsModalService.onHidden.subscribe((reason: any) => {
-        if(reason === "CERRAR"){
-          observer.error();
-        }else{
-          observer.complete();
-        }
-      });
-
-      return {
-        unsubscribe() {
-          subscripcion.unsubscribe();
-        }
-      }
-    });
-  }
-
-  modalEscribirRechazo(obj: ISolicitudPropiedad): Observable<any> {
-    const config: ModalOptions = {
-      initialState:  {
-        input_sol_prop: obj
-      },
-      animated: true,
-      ignoreBackdropClick: true,
-      backdrop: "static",
-      keyboard: false
-    }
-    this.modalRef = this.bsModalService.show(RechazarSolicitudComponent, config);
-    return new Observable<any>(observer => {
-      const subscripcion = this.bsModalService.onHidden.subscribe((reason: any) => {
-        if(reason === "CERRAR"){
-          observer.error();
-        }else{
-          observer.complete();
-        }
-      });
-
-      return {
-        unsubscribe() {
-          subscripcion.unsubscribe();
-        }
-      }
-    });
-  }*/
+//
 
 }
