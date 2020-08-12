2fc8c8715c1377be0dc50663619597e92f6c2ac5
juniormorales <angel_96vir@hotmail.com>
2020-08-12 16:25:48 -0500

    correccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,19 +3,19 @@ export const environment = {
   production: false,
   msg_servicio_no_disponible: 'Servicio no disponible, intentelo más tarde nuevamente.',
   
-  
+  /*
   urlLocalHost: 'http://localhost:2000',
   urlBack: 'http://localhost:2000/',
   urlOauth: 'http://localhost:2000/oauth/token',
   urlApiRest: 'http://localhost:2000/api/',
   urlPublicRest: 'http://localhost:2000/public/',
+  */
   
-  /*
  urlLocalHost: 'https://rentame-back.herokuapp.com',
   urlBack: 'https://rentame-back.herokuapp.com/',
   urlOauth: 'https://rentame-back.herokuapp.com/oauth/token',
   urlApiRest: 'https://rentame-back.herokuapp.com/api/',
   urlPublicRest: 'https://rentame-back.herokuapp.com/public/',
-*/
+
 
 };
