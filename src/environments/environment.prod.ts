318dac8215a3dd2483bda209c15c9c12a87a3658
juniormorales <angel_96vir@hotmail.com>
2021-07-10 21:37:51 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   msg_servicio_no_disponible: 'Servicio no disponible, intentelo más tarde nuevamente.',
-  urlLocalHost: 'https://rentame-back.herokuapp.com',
-  urlBack: 'https://rentame-back.herokuapp.com/',
-  urlOauth: 'https://rentame-back.herokuapp.com/oauth/token',
-  urlApiRest: 'https://rentame-back.herokuapp.com/api/',
-  urlPublicRest: 'https://rentame-back.herokuapp.com/public/',
+  urlLocalHost: 'https://leaseme-backend.herokuapp.com/',
+  urlBack: 'https://leaseme-backend.herokuapp.com/',
+  urlOauth: 'https://leaseme-backend.herokuapp.com/oauth/token',
+  urlApiRest: 'https://leaseme-backend.herokuapp.com/api/',
+  urlPublicRest: 'https://leaseme-backend.herokuapp.com/public/',
 };
